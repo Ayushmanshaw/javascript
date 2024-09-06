3d293ccbb32ea6068f53a63410f61a76a6ed8179
Ayushmanshaw <114843319+Ayushmanshaw@users.noreply.github.com>
2024-09-06 18:03:10 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 123456;
+let accountEmail = "ayushmanshaw@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+//accountId = 8765; ypu cant assign const variables
+console.log(accountId);
+console.log( accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
