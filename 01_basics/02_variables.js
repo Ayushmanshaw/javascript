429a5582d3dd20c2684c8eb0239bfe6714f46986
Ayushmanshaw <114843319+Ayushmanshaw@users.noreply.github.com>
2024-09-06 18:00:41 +0000

    datatypes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,11 @@
+//prefer not to use var variable due to global andblock scope functionality
+"use strict" //treat all JS code an newer version
+//alert(3+3); //we are using nodejs not browser
+console.log(3);
+//number
+//null
+//boolean
+//bigint
+//string
+//symbol
+//undefined
