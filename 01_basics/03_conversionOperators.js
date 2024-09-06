066265bb4b9f253483bd439dfb58491dd5486c95
Ayushmanshaw <114843319+Ayushmanshaw@users.noreply.github.com>
2024-09-06 19:17:53 +0000

    operatords

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,25 @@
+let score= "54";
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+let s= "54abc";
+console.log(typeof s);
+console.log(typeof (s));
+let value = Number(s);
+console.log(typeof value);
+console.log(value);
+//++++++++++++++++++++operations++++++++++++++++++++++++++
+let val=8;
+let negvalue=-val;
+console.log(val);
+console.log(negvalue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+"2");
+
